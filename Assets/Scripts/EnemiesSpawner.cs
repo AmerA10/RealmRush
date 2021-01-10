399d325ca9c003bf492997c2bf365e8a7d664404
AmerA10 <ajami.amer1@gmail.com>
2021-01-10 01:02:49 -0700

    spawningSetUp

---
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawner : MonoBehaviour
+{
+
+    [SerializeField] float secondsBetweenSpawns = 2f;
+    [SerializeField] EnemyMover enemy;
+    [SerializeField] bool isSpawning = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnEnemies());   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator spawnEnemies()
+    {
+        while (isSpawning)
+        {
+            Instantiate(enemy, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+
+    }
+}
